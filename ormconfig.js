fe88163559578d790482efaae9a753f7caa51b7f
Jonathan Zanella <jonathanrafaelz@gmail.com>
2018-03-15 21:37:25 -0300

    Create circle configuration file

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,11 @@
 module.exports = {
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "",
-  password: "",
-  database: process.env.NODE_ENV === "test" ? "myinvestments-test" : "myinvestments",
+  host: process.env.POSTGRES_HOST || "localhost",
+  port: process.env.POSTGRES_PORT || 5432,
+  username: process.env.POSTGRES_USER || "",
+  password: process.env.POSTGRES_PASSWORD || "",
+  database:
+    process.env.NODE_ENV === "test" ? "myinvestments-test" : "myinvestments",
   dropSchema: process.env.NODE_ENV === "test",
   synchronize: true,
   logging: false,
