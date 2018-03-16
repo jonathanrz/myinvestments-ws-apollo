0c33b1e45867eb8108332bd3d2ce4d9721a2cb24
Jonathan Zanella <jonathanrafaelz@gmail.com>
2018-03-16 20:51:26 -0300

    create POSTGRES_DB_NAME env var

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,14 @@
+const database = process.env.POSTGRES_DB_NAME || "myinvestments"
+const isTestEnv = process.env.NODE_ENV === "test"
+
 module.exports = {
   type: "postgres",
   host: process.env.POSTGRES_HOST || "localhost",
   port: process.env.POSTGRES_PORT || 5432,
   username: process.env.POSTGRES_USER || "",
   password: process.env.POSTGRES_PASSWORD || "",
-  database:
-    process.env.NODE_ENV === "test" ? "myinvestments-test" : "myinvestments",
-  dropSchema: process.env.NODE_ENV === "test",
+  database: isTestEnv ? `${database}-test` : database,
+  dropSchema: isTestEnv,
   synchronize: true,
   logging: false,
   entities: ["src/entity/**/*.ts"],
