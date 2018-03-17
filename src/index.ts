bb15c143fd81d64943264f211f9d83c963360ac5
Jonathan Zanella <jonathanrafaelz@gmail.com>
2018-03-16 21:14:48 -0300

    change running text

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
-import "reflect-metadata";
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import { get } from "lodash";
-import { GraphQLServer } from "graphql-yoga";
-import { createConnection } from "typeorm";
-import { schema } from "./schema";
-import { authentication, ensureAuth } from "./authentication";
+import "reflect-metadata"
+import * as express from "express"
+import * as bodyParser from "body-parser"
+import { get } from "lodash"
+import { GraphQLServer } from "graphql-yoga"
+import { createConnection } from "typeorm"
+import { schema } from "./schema"
+import { authentication, ensureAuth } from "./authentication"
 
-const PORT: number = parseInt(process.env.port, 10) || 4000;
+const PORT: number = parseInt(process.env.PORT, 10) || 4000
 
 createConnection().then(() => {
   const server = new GraphQLServer({
     schema,
     context: ({ request }) => {
-      const user = get(request, "session.passport.user");
+      const user = get(request, "session.passport.user")
 
       return {
         user,
         ensureAuth: ensureAuth(user)
-      };
+      }
     }
-  });
+  })
 
-  server.express.use(bodyParser.urlencoded({ extended: false }));
-  server.express.use(bodyParser.json());
+  server.express.use(bodyParser.urlencoded({ extended: false }))
+  server.express.use(bodyParser.json())
 
-  authentication(server.express);
+  authentication(server.express)
 
   server.start({ port: PORT }, () =>
-    console.log(`Server is running on localhost:${PORT}`)
-  );
-});
+    console.log(`Server is running on port ${PORT}`)
+  )
+})
