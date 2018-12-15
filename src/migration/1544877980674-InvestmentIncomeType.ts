cfab1264daa5553e04bcc874d474eb71625fa734
Jonathan Zanella <jonathanrafaelz@gmail.com>
2018-12-15 10:52:30 -0200

    fix lint

--- src/migration/1544877980674-InvestmentIncomeType.ts
+++ src/migration/1544877980674-InvestmentIncomeType.ts
@@ -1,13 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class InvestmentIncomeType1544877980674 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `ALTER TABLE "Investment" ADD COLUMN "incomeType" VARCHAR`
+    )
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-      await queryRunner.query(`ALTER TABLE "Investment" ADD COLUMN "incomeType" VARCHAR`);
-    }
- 
-    public async down(queryRunner: QueryRunner): Promise<any> {
-      await queryRunner.query(`ALTER TABLE "Investment" DROP COLUMN "incomeType"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`ALTER TABLE "Investment" DROP COLUMN "incomeType"`)
+  }
 }
